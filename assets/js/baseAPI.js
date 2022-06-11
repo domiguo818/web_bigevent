0f8903e4f457336999727db1e0cafc14f8bdd5f9
domiguo818 <1193224158@qq.com>
2022-06-11 09:48:50 +0800

    完成了主页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,17 @@
 $.ajaxPrefilter((Option)=>{
     Option.url =`http://www.liulongbin.top:3007`+ Option.url
+    if(Option.url.includes("/my/")){
+        Option.headers = {
+            Authorization:localStorage.getItem("token")
+        }
+    }
+    Option.complete =(res) =>{
+        if(
+            res.responseJSON.status === 1&&
+            res.responseJSON.message === "身份认证失败！"
+        ){
+            localStorage.removeItem("token");
+            location.href ="/login.html"
+        }
+    }
 })
